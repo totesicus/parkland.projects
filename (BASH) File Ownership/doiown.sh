c794bf589f67ec8a9a80907f2327bcbda4361bd7
Tyler Turner <totesicus@gmail.com>
2017-11-01 23:03:16 -0500

    (BASH) File Ownership [cmdline]
    Changed the function of this script to be run directly from the command
    line.

--- (BASH) File Ownership/doiown.sh
+++ (BASH) File Ownership/doiown.sh
@@ -6,16 +6,12 @@ err_fn () {
 	exit 10
 }
 
-echo Please enter the name of a file:
-
-read userinput
-
-if [ -O $userinput  ]
+if [ -O $1  ]
 	then
 		echo The file is there and you own it.
-	elif [ -e $userinput ]
+	elif [ -e $1 ]
 		then
-			err_fn "The file was found, but you do not own it. Please try again." "You entered $userinput."
+			err_fn "The file was found, but you do not own it. Please try again." "You entered $1."
 	else
-		err_fn "The file was not found, please try again." "You entered $userinput"
+		err_fn "The file was not found, please try again." "You entered $1"
 fi
