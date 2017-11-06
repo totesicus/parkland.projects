df2ca1630531e73264693ca8d7095d0bcb62c3f2
Tyler Turner <totesicus@gmail.com>
2017-11-06 17:08:22 -0600

    (C#) PayRateCalculator

--- (C#) PayRateCalculator/PayRate(Console)/Program.cs
+++ (C#) PayRateCalculator/PayRate(Console)/Program.cs
@@ -10,35 +10,53 @@ namespace PayRate_Console_
     {
         static void Main(string[] args)
         {
+            // String variables for user input
             String hoursIn, payRateIn, taxIn, response;
+
+            // Double variables for parsed String output
             double hours, payRate, payRateResult, tax, taxResult, result;
+
+            // Boolean variable for loop
             Boolean loop = true;
 
             do
-            {
+            {   
+                // Greet the user
                 Console.Out.WriteLine("Welcome!\n");
-                Console.Out.WriteLine("Hours worked:");
 
+                // Take user input in String format 
+                Console.Out.WriteLine("Hours worked:");           
                 hoursIn = Console.ReadLine();
+
+                // Parse to type double
                 hours = double.Parse(hoursIn);
 
+                // Take user input in String format 
                 Console.Out.WriteLine("\nPay rate:");
-
                 payRateIn = Console.ReadLine();
+
+                // Parse to type double
                 payRate = double.Parse(payRateIn);
 
+                // Take user input in String format 
                 Console.Out.WriteLine("\nTax rate:");
-
                 taxIn = Console.ReadLine();
+                // Parse to type double
                 tax = double.Parse(taxIn);
 
+                // Make calculation
+                // Find amount in taxes taken out of pay rate
+                // Subtract that amount from pay rate
+                // Calculate approximate pay by multiplying taxed pay rate by hours worked
                 taxResult = tax * payRate;
                 payRateResult = payRate - taxResult;
                 result = hours * payRateResult;
 
+                // Output approximate pay in currency format
                 Console.Out.WriteLine("\nYour paycheck for the week will be (approx): " +
                             result.ToString("C"));
 
+                // Continue or end the loop via user input
                 Console.Out.WriteLine("Make another calculation? (y/n)");
                 response = Console.ReadLine();
 
