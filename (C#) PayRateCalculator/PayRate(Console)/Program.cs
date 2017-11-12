f888741f6e2a28a892bff506950dd900cd0f60bd
Tyler Turner <totesicus@gmail.com>
2017-11-11 21:00:34 -0600

    (C#) PayRateCalculator

--- (C#) PayRateCalculator/PayRate(Console)/Program.cs
+++ (C#) PayRateCalculator/PayRate(Console)/Program.cs
@@ -29,6 +29,7 @@ namespace PayRate_Console_
                 // Take user input in String format 
                 Console.Out.WriteLine("Hours worked:");           
                 hoursIn = Console.ReadLine();
+                
                 test.InputTest(hoursIn);
 
                 // Parse to type double
